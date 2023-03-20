15d1e0e12161cf5556e89b02c68e2a6e270eef42
nramltpr <nfzptr@gmail.com>
2023-03-20 16:20:45 +0700

    update sga

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -126,7 +126,7 @@
                 </svg>
               </span> -->
               <div class="avatar">
-                <img src="<?php echo base_url('assets/sneatadmin/assets');?>/img/avatars/logo k3 hijau.png" alt class="w-px-40 h-auto rounded-circle" />
+                <img src="<?php echo base_url('assets/sneatadmin/assets');?>/img/avatars/logok3hijau.png" alt class="w-px-40 h-auto rounded-circle" />
               </div>
               <span class="app-brand-text demo menu-text fw-bolder ms-2">SGA System</span>
             </a>
@@ -264,7 +264,7 @@
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                   <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                      <img src="<?php echo base_url('assets/sneatadmin/assets');?>/img/avatars/logo k3 hijau.png" alt class="w-px-40 h-auto rounded-circle" />
+                      <img src="<?php echo base_url('assets/sneatadmin/assets');?>/img/avatars/logok3hijau.png" alt class="w-px-40 h-auto rounded-circle" />
                     </div>
                   </a>
                   <ul class="dropdown-menu dropdown-menu-end">
@@ -273,7 +273,7 @@
                         <div class="d-flex">
                           <div class="flex-shrink-0 me-3">
                             <div class="avatar avatar-online">
-                              <img src="<?php echo base_url('assets/sneatadmin/assets');?>/img/avatars/logo k3 hijau.png" alt class="w-px-40 h-auto rounded-circle" />
+                              <img src="<?php echo base_url('assets/sneatadmin/assets');?>/img/avatars/logok3hijau.png" alt class="w-px-40 h-auto rounded-circle" />
                             </div>
                           </div>
                           <div class="flex-grow-1">
